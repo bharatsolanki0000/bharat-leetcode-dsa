f39abdc0b9c3df6e56c247161ee624de63d0a96d
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-02-19 14:16:45 +0530

    Time: 79 ms (81.23%), Space: 15.6 MB (75.16%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -42,6 +42,55 @@ class Solution {
 
     }
 
+    bool tabuSolve(vector<int> &nums, int Target){
+        vector<vector<int>> dp(nums.size()+1, vector<int> (Target+1,0));
+
+       for(int i=0;i<=nums.size();i++){
+            dp[i][0]=1;
+       } 
+
+        for(int index=nums.size()-1;index>=0;index--){
+            for(int target=1;target<=Target;target++){
+                //take
+                bool take=false;
+                if(target>=nums[index]){
+                    take=dp[index+1][target-nums[index]];
+                }
+              
+                bool notTake=dp[index+1][target];
+
+                dp[index][target]= take||notTake;
+            }
+        }
+
+        return dp[0][Target];
+    }
+
+    bool soSolve(vector<int> &nums, int Target){
+       vector<int> curr(Target+1, 0);
+       vector<int> next(Target+1, 0);
+
+       curr[0]=1;
+       next[0]=1;
+
+        for(int index=nums.size()-1;index>=0;index--){
+            for(int target=1;target<=Target;target++){
+                //take
+                bool take=false;
+                if(target>=nums[index]){
+                    take=next[target-nums[index]];
+                }
+              
+                bool notTake=next[target];
+
+               curr[target]= take||notTake;
+            }
+            next=curr;
+        }
+
+        return next[Target];
+    }
+
 public:
     bool canPartition(vector<int>& nums) {
         int target=0;
@@ -60,8 +109,14 @@ public:
 
 
         //rec +memo
-        vector<vector<int>> dp(nums.size(), vector<int> ((target/2)+1, -1));
-        return dpSolve(nums, target/2,0, dp);
+        // vector<vector<int>> dp(nums.size(), vector<int> ((target/2)+1, -1));
+        // return dpSolve(nums, target/2,0, dp);
+
+        //tabu solve
+        //return tabuSolve(nums, target/2);
+
+        //SO
+        return soSolve(nums,target/2);
        
         
     }
