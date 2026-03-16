a2f4ee3da9b99687cd8c2162b5990d374ea46cc2
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-03-16 18:12:47 +0530

    Time: 70 ms (21.12%), Space: 54.6 MB (19.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -45,7 +45,7 @@ class Solution {
 
             for(int k=j;k<p.length();k++){
                 if(p[k]!='*'){
-                    //valid
+                    //invalid
                     return false;
                 }
             }
@@ -76,13 +76,53 @@ class Solution {
     }
 
 
+    bool tabuSolve(string &s, string &p){
+        vector<vector<int>> dp(s.length()+1, vector<int> (p.length()+1,0));
+
+        dp[s.length()][p.length()]=1;
+  
+            for(int j=p.length()-1;j>=0;j--){
+                if(p[j]=='*'){
+                    dp[s.length()][j] = 1;
+                }
+                else{
+                    break;
+                }
+            }
+
+        for(int i=s.length()-1;i>=0;i--){
+
+            for(int j=p.length()-1;j>=0;j--){
+
+                
+                bool ans=false;
+                if(s[i]==p[j] || p[j]=='?'){
+                    ans=dp[i+1][j+1];
+                }
+                else if(p[j]=='*'){
+                    bool letEmpty=dp[i][j+1];
+                    bool letChar=dp[i+1][j];
+
+                    ans=letEmpty || letChar;
+                }
+
+                dp[i][j]=ans;
+
+            }
+        }
+
+        return dp[0][0];
+    }
+
 
 public:
     bool isMatch(string s, string p) {
         //return solve(s,p,0,0);
 
         //rec +memo
-        vector<vector<int>> dp(s.length()+1, vector<int> (p.length()+1,-1));
-        return dpSolve(s,p,0,0,dp);
+        // vector<vector<int>> dp(s.length()+1, vector<int> (p.length()+1,-1));
+        // return dpSolve(s,p,0,0,dp);
+
+        return tabuSolve(s,p);
     }
 };
