a75cf7ed2fcd526309f4fa9de6653b79da27448f
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-03-20 16:47:19 +0530

    Time: 36 ms (23.63%), Space: 21.7 MB (15.17%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,94 @@
+class Solution {
+
+    int solve(vector<int>& nums, int index){
+        if(index>=nums.size()-1){
+            return 0;
+        }
+
+        int steps=INT_MAX;
+
+        for(int i=index+1;i<=index+nums[index];i++){
+            
+            
+            int tempSteps=solve(nums,i);
+
+            if(tempSteps!=INT_MAX){
+                steps=min(steps, 1+tempSteps);
+            }
+
+        }
+
+        return steps;
+
+
+    }
+
+    int dpSolve(vector<int>& nums, int index, vector<int>&dp){
+        if(index>=nums.size()-1){
+            return 0;
+        }
+
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+
+        int steps=INT_MAX;
+
+        for(int i=index+1;i<=index+nums[index];i++){
+            
+            
+            int tempSteps=dpSolve(nums,i,dp);
+
+            if(tempSteps!=INT_MAX){
+                steps=min(steps, 1+tempSteps);
+            }
+
+        }
+
+        return dp[index]=steps;
+
+
+    }
+
+
+    int tabuSolve(vector<int>&nums){
+
+        vector<int> dp(nums.size(),INT_MAX);
+
+        dp[nums.size()-1]=0;
+
+        for(int index=nums.size()-2;index>=0;index--){
+              int steps=INT_MAX;
+                for(int i=index+1;i<=index+nums[index];i++){
+                    
+
+                    int tempSteps=INT_MAX;
+
+                    if(i< nums.size()){
+                        tempSteps=dp[i];
+                    }  
+
+                    if(tempSteps!=INT_MAX){
+                        steps=min(steps, 1+tempSteps);
+                    }
+
+                }
+                 dp[index]=steps;
+        }
+        return dp[0];
+    }
+
+public:
+    int jump(vector<int>& nums) {
+
+        if(nums.size()==1){
+            return 0;
+        }
+        //return solve(nums,0);
+
+        // vector<int> dp(nums.size(),-1);
+        // return dpSolve(nums,0,dp);
+
+        return tabuSolve(nums);
+    }
+};
