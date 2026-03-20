4a0e12dd4af897df879b1720bee32fb17a100688
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-03-20 18:55:53 +0530

    Time: 0 ms (100%), Space: 20.5 MB (61.53%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -78,6 +78,28 @@ class Solution {
         return dp[0];
     }
 
+    int greedySolve(vector<int>& nums){
+
+        int l=0;
+        int r=0;
+        int steps=0;
+        
+
+        while(r<nums.size()-1){
+            int farthest=0;
+
+            for(int i=l;i<=r;i++){
+                farthest=max(farthest, i+nums[i]);
+            }
+
+            l=r+1;
+            steps++;
+            r=farthest;
+        }
+    return steps;
+
+    }
+
 public:
     int jump(vector<int>& nums) {
 
@@ -89,6 +111,7 @@ public:
         // vector<int> dp(nums.size(),-1);
         // return dpSolve(nums,0,dp);
 
-        return tabuSolve(nums);
+        //return tabuSolve(nums);
+        return greedySolve(nums);
     }
 };
