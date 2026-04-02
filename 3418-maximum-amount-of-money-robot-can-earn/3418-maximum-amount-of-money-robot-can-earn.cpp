ebfe083922438f153bccba8a4703cb8397bd8952
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-04-02 23:41:28 +0530

    Time: 537 ms (53.62%), Space: 277.2 MB (14.04%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -14,49 +14,31 @@ int solve(vector<vector<int>>& coins, int row, int col, int &rowSize, int &colSi
         return INT_MIN;
       }
 
-        int amount=INT_MIN;
+        int skipCoin=INT_MIN;
+        int takeCoin=INT_MIN;
 
-        int right=solve(coins, row, col+1, rowSize, colSize, protect);
-        int down=solve(coins, row+1, col, rowSize, colSize, protect);
+        //TAKE
+        int takeRight=solve(coins, row, col+1, rowSize, colSize,protect);
+        int takeDown=solve(coins, row+1, col, rowSize, colSize, protect);
+        takeCoin=coins[row][col]+max(takeRight, takeDown);
 
-        // RIGHT PATH
-        if(right!=INT_MIN){
+        //skip 
+        if(coins[row][col]<0 && protect>0){
+             int skipRight=solve(coins, row, col+1, rowSize, colSize,protect-1);
+             int skipDown=solve(coins, row+1, col, rowSize, colSize, protect-1);
 
-            if(coins[row][col]<0){
-                if(protect>0){
-                    amount = max(amount, solve(coins, row, col+1, rowSize, colSize, protect-1)); 
-                }
-              
-                amount = max(amount, right + coins[row][col]);
-            }
-            else{
-                amount = max(amount, right + coins[row][col]);
-            }
+             skipCoin=max(skipRight, skipDown);
         }
+        
+        return max(takeCoin, skipCoin);
 
-        // DOWN PATH
-        if(down!=INT_MIN){
-
-            if(coins[row][col]<0){
-               
-                if(protect>0){
-                    amount = max(amount, solve(coins, row+1, col, rowSize, colSize, protect-1));
-                }
-                
-                amount = max(amount, down + coins[row][col]);
-            }
-            else{
-                amount = max(amount, down + coins[row][col]);
-            }
-        }
-
-        return amount;
+        
     }
 
     
 int dpSolve(vector<vector<int>>& coins, int row, int col, int &rowSize, int &colSize, int protect, vector<vector<vector<int>>> &dp){
 
-      if(row==rowSize-1 && col==colSize-1){
+       if(row==rowSize-1 && col==colSize-1){
 
             if(protect>0 && coins[row][col]<0){
                 return 0;
@@ -68,62 +50,44 @@ int dpSolve(vector<vector<int>>& coins, int row, int col, int &rowSize, int &col
         return INT_MIN;
       }
 
-      if(dp[row][col][protect]!=-1){ 
+      if(dp[row][col][protect]!=-1){
         return dp[row][col][protect];
       }
 
-        int amount=INT_MIN;
+        int skipCoin=INT_MIN;
+        int takeCoin=INT_MIN;
 
-        int right=dpSolve(coins, row, col+1, rowSize, colSize, protect,dp);
-        int down=dpSolve(coins, row+1, col, rowSize, colSize, protect,dp);
+        //TAKE
+        int takeRight=dpSolve(coins, row, col+1, rowSize, colSize,protect,dp);
+        int takeDown=dpSolve(coins, row+1, col, rowSize, colSize, protect,dp);
+        takeCoin=coins[row][col]+max(takeRight, takeDown);
 
-        // RIGHT PATH
-        if(right!=INT_MIN){
+        //skip 
+        if(coins[row][col]<0 && protect>0){
+             int skipRight=dpSolve(coins, row, col+1, rowSize, colSize,protect-1,dp);
+             int skipDown=dpSolve(coins, row+1, col, rowSize, colSize, protect-1,dp);
 
-            if(coins[row][col]<0){
-                if(protect>0){
-                    amount = max(amount, dpSolve(coins, row, col+1, rowSize, colSize, protect-1,dp)); 
-                }
-              
-                amount = max(amount, right + coins[row][col]);
-            }
-            else{
-                amount = max(amount, right + coins[row][col]);
-            }
+             skipCoin=max(skipRight, skipDown);
         }
+        
+        return dp[row][col][protect]=max(takeCoin, skipCoin);
 
-        // DOWN PATH
-        if(down!=INT_MIN){
-
-            if(coins[row][col]<0){
-               
-                if(protect>0){
-                    amount = max(amount, dpSolve(coins, row+1, col, rowSize, colSize, protect-1,dp));
-                }
-                
-                amount = max(amount, down + coins[row][col]);
-            }
-            else{
-                amount = max(amount, down + coins[row][col]);
-            }
-        }
-
-        return dp[row][col][protect]=amount;
+        
     }
 
 int tabuSolve(vector<vector<int>>& coins){
     int rowSize = coins.size(); 
     int colSize = coins[0].size();
 
-    vector<vector<vector<int>>> dp(rowSize, vector<vector<int>>(colSize, vector<int>(3, INT_MIN)));
+    vector<vector<vector<int>>> dp(rowSize+1, vector<vector<int>>(colSize+1, vector<int>(3, INT_MIN)));
     
     
-    for(int p = 0; p < 3; p++){
-        if(coins[rowSize-1][colSize-1] < 0){
-            if(p > 0) dp[rowSize-1][colSize-1][p] = 0;
-            else dp[rowSize-1][colSize-1][p] = coins[rowSize-1][colSize-1];
-        } else {
-            dp[rowSize-1][colSize-1][p] = coins[rowSize-1][colSize-1];
+    for(int p=0;p<3;p++){
+        if(coins[rowSize-1][colSize-1] <0 && p>0){
+            dp[rowSize-1][colSize-1][p]=0;
+        }
+        else{
+             dp[rowSize-1][colSize-1][p]=coins[rowSize-1][colSize-1];
         }
     }
 
@@ -135,52 +99,25 @@ int tabuSolve(vector<vector<int>>& coins){
 
             for(int protect = 0; protect < 3; protect++){
 
-                int amount = INT_MIN;
+                    
+                int skipCoin=INT_MIN;
+                int takeCoin=INT_MIN;
 
-                int right = (col+1 < colSize) ? dp[row][col+1][protect] : INT_MIN;
-                int down  = (row+1 < rowSize) ? dp[row+1][col][protect] : INT_MIN;
+                //TAKE
+                int takeRight=dp[row][col+1][protect];
+                int takeDown=dp[row+1][col][protect]; 
+                takeCoin=coins[row][col]+max(takeRight, takeDown);
 
-                // 🔹 RIGHT
-                if(right != INT_MIN){
+                //skip 
+                if(coins[row][col]<0 && protect>0){
+                    int skipRight=dp[row][col+1][protect-1];
+                    int skipDown=dp[row+1][col][protect-1];
 
-                    if(coins[row][col] < 0){
-
-                        // use protection
-                        if(protect > 0){
-                            int val = dp[row][col+1][protect-1];
-                            if(val != INT_MIN)
-                                amount = max(amount, val);
-                        }
-
-                        // don't use protection
-                        amount = max(amount, right + coins[row][col]);
-                    }
-                    else{
-                        amount = max(amount, right + coins[row][col]);
-                    }
+                    skipCoin=max(skipRight, skipDown);
                 }
+                
+                dp[row][col][protect]=max(takeCoin, skipCoin);
 
-                // 🔹 DOWN
-                if(down != INT_MIN){
-
-                    if(coins[row][col] < 0){
-
-                        // use protection
-                        if(protect > 0){
-                            int val = dp[row+1][col][protect-1];
-                            if(val != INT_MIN)
-                                amount = max(amount, val);
-                        }
-
-                        // don't use protection
-                        amount = max(amount, down + coins[row][col]);
-                    }
-                    else{
-                        amount = max(amount, down + coins[row][col]);
-                    }
-                }
-
-                dp[row][col][protect] = amount;
             }
         }
     }
@@ -195,7 +132,7 @@ public:
         int colSize=coins[0].size();
 
         int protect=2;
-        //return solve(coins, 0,0, rowSize, colSize, protect);
+      //  return solve(coins, 0,0, rowSize, colSize, protect);
 
         // vector<vector<vector<int>>> dp(rowSize+1, vector<vector<int>>(colSize+1, vector<int> (3,-1)));
         // return dpSolve(coins, 0,0, rowSize, colSize, protect, dp);
