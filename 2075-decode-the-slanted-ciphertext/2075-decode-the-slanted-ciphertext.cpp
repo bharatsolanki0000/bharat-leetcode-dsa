a3d4c644d4058f476f270cd3a46c6a529319b991
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-04-04 18:16:45 +0530

    Time: 64 ms (5.93%), Space: 46.1 MB (44.07%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -7,38 +7,20 @@ public:
 
         int cols=encodedText.length()/rows;
 
-        vector<vector<char>> grid(rows, vector<char> (cols,' '));
-
-        int index=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                grid[i][j]=encodedText[index++];
-            }
-        }
-
         
+        int size=encodedText.length();
         string ans="";
-        
 
-        int i=0;
-        int j=0;
-        int k=0;
-        int flag=false;
-        while(k<cols){
-
-            while(i<rows && j<cols){
-                ans.push_back(grid[i++][j++]); 
+        for(int j=0;j<cols;j++){
+            for(int index=j;index<encodedText.length();index=index+cols+1){
+                ans.push_back(encodedText[index]);
             }
-
-            i=0;
-            k++;
-            j=k;
-
         }
 
         while(ans[ans.length()-1]==' '){
             ans.pop_back();
         }
+        
 
         return ans;
     }
