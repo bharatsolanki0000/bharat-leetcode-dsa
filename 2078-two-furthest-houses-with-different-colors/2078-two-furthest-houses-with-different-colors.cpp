e45e08b23a33b4518f130b2a942f49ca7bf61c33
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-04-20 17:07:54 +0530

    Time: 0 ms (100%), Space: 12 MB (72.82%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -2,24 +2,41 @@ class Solution {
 public:
     int maxDistance(vector<int>& colors) {
         
-        //two pass
-        // from leftmost to farthest right
-    int ans=0;
+        // //two pass
+        // // from leftmost to farthest right
+        // int ans=0;
       
-        int j=colors.size()-1;
-        while(j>0 && colors[j]==colors[0]){
-            j--;
+        // int j=colors.size()-1;
+        // while(j>0 && colors[j]==colors[0]){
+        //     j--;
+        // }
+
+        // ans=max(ans, abs(j));
+
+        // int i=0;
+        // while(i<colors.size() && colors[i]==colors[colors.size()-1]){
+        //     i++;
+        // }
+        
+        // ans=max(ans, (int)colors.size()-i-1);
+        
+        // return ans;
+
+
+        //one pass
+        int ans=0;
+
+        for(int i=0;i<colors.size();i++){
+
+            if(colors[i]!=colors[0]){
+                ans=max(ans, abs(i));
+            }
+
+            if(colors[i]!=colors[colors.size()-1]){
+                ans=max(ans, abs((int)colors.size()-i-1));
+            }
         }
 
-        ans=max(ans, abs(j));
-
-        int i=0;
-        while(i<colors.size() && colors[i]==colors[colors.size()-1]){
-            i++;
-        }
-        
-        ans=max(ans, (int)colors.size()-i-1);
-        
         return ans;
 
     }
