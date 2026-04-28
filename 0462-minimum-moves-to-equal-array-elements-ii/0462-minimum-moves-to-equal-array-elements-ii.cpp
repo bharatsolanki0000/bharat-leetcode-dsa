8c5a696f29ffaa5baab0521bcdc0d9002ffa50ab
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-04-28 23:20:34 +0530

    Time: 0 ms (100%), Space: 15.7 MB (52.83%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
     typedef long long ll;
-public:
-    int minMoves2(vector<int>& nums) {
+
+    int brute(vector<int>&nums){
         sort(nums.begin(), nums.end());
         
         ll ans=LLONG_MAX;
@@ -29,5 +29,54 @@ public:
         }
 
         return ans;
+    }
+
+    int better(vector<int>& nums){
+  
+
+        sort(nums.begin(), nums.end());
+
+        //see code with alisha youtube
+        //we want to min the moves . so we need to choose middle element
+        // 1 3 7 8 11
+        //here middle is 7 so we will get moves to make every element as 7. 
+        // in left of 7 . we do (nums[left]+x)=7   where x is added to nums[i] to make it 7 so(nums[left]+x)
+
+        //in right of 7 . we do (nums[right]-y)=7   where y is sub to nums[i] to make it 7 so(nums[left]-y)
+        
+        // and we know both eq wants to become 7 so
+
+        //left = right= 7
+        //nums[left]+x = nums[right]-y = 7 
+        //which gives ,
+        //x+y= nums[right]-nums[left];
+
+
+
+        int left=0;
+        int right=nums.size()-1;
+        ll ans=0;
+
+        while(left<=right){
+            
+            if((nums[left]>0 && nums[right]>0)  || (nums[left]<0 && nums[right]<0)){
+                ans+=abs(abs(nums[right])- abs(nums[left]));
+            }
+            else{
+                ans+=abs(abs(nums[right])+ abs(nums[left]));
+            }
+
+            left++;
+            right--;
+        }
+
+
+        return ans;
+        }
+public:
+    int minMoves2(vector<int>& nums) {
+        //return brute(nums);
+
+        return better(nums);
     }
 };
