40a406f357175e1802129782b80a4ce354b40cbb
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-04-28 19:40:15 +0530

    Time: 1216 ms (7.59%), Space: 15.7 MB (52.83%) - LeetHub

---
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    typedef long long ll;
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        ll ans=LLONG_MAX;
+
+        for(int i=0;i<nums.size();i++){
+
+            ll moves=0;
+            for(int j=0;j<nums.size();j++){
+                
+                if(nums[i]<0 && nums[j]>0){
+                    moves+=abs(nums[i])+nums[j];
+                }
+                else if(nums[i]>0 && nums[j]<0){
+                    moves+=abs(nums[j])+nums[i];
+                }
+                else{
+
+                    moves+=abs(abs(nums[i])-abs(nums[j]));
+                }
+            }
+            ans=min(ans,moves);
+        }
+
+        return ans;
+    }
+};
