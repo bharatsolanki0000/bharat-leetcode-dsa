68840286d7e20d4e1804a5385a27755212ccef58
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-05-27 22:34:24 +0530

    Time: 66 ms (77.25%), Space: 28 MB (6.18%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
-public:
-    int numberOfSpecialChars(string word) {
-        
+
+    int hashMap(string word){
         unordered_map<char,int> lower, upper;
         int ans = 0;
 
@@ -38,5 +37,54 @@ public:
         }
 
         return ans;
+    }
+
+    int freqVector(string word){
+
+        vector<int> lower(26,INT_MIN);
+        vector<int> upper(26,INT_MAX);
+
+        int ans=0;
+
+       
+        for(int i=0;i<word.length();i++){
+
+            char element=word[i];
+
+
+            //lower char last occurance
+            if(element>='a' && element<='z'){
+                lower[element-'a']=i;
+            }
+            else{
+                //first occurance of upper letter
+
+                if(upper[element-'A']==INT_MAX){
+                    upper[element-'A']=i;
+                }
+            }
+
+        }
+
+        for(int i=0;i<26;i++){
+
+            if(lower[i]!=INT_MIN && upper[i]!=INT_MAX){
+
+                if(lower[i]<upper[i]){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+
+
+    }
+public:
+    int numberOfSpecialChars(string word) {
+        //return hashMap(word);
+
+        return freqVector(word);
+        
     }
 };
