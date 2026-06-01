02f974d3d0cb0c511948aef833725301f8055efb
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-06-02 00:43:09 +0530

    Time: 3 ms (18.81%), Space: 8.8 MB (80.36%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        int count=0;
+        string ans="";
+
+        for(int i=0;i<s.length();i++){
+
+            if(s[i]=='('){
+
+               if(count>0){
+                 ans.push_back('(');
+               }
+               count++;
+              
+            
+
+            }
+            else{
+
+                count--;
+
+                if(count!=0){
+                    ans.push_back(')');
+                }
+
+                
+            }
+        }
+        return ans;
+    }
+};
