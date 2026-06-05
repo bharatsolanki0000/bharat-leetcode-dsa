8a57a98e505dddf6127aa14d6530ddc79f377d30
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-06-06 01:53:25 +0530

    Time: 0 ms (100%), Space: 15 MB (24.4%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -9,43 +9,86 @@
  * };
  */
 class Solution {
+
+    ListNode* myApproach(ListNode* head, int n){
+        
+        if(head->next==nullptr ){
+           return nullptr;
+        }
+
+        if(head->next->next==nullptr){
+            //2 nodes
+
+            if(n==1){
+                head->next=nullptr;
+                return head;
+            }
+
+            if(n==2){
+                head=head->next;
+                return head;
+            }
+        }
+
+        ListNode* first=head,*second=head;
+
+        int count=0;
+        while(first && count<n){
+            first=first->next;
+            count++;
+        }
+
+        if(first==nullptr){
+            head=head->next;
+            return head;
+        }
+
+        while(first->next){
+            second=second->next;
+            first=first->next;
+        }
+
+        if(second->next==first){
+            second->next=nullptr;
+            return head;
+        }
+
+        ListNode* tempp=second->next->next;
+        second->next=nullptr;
+        second->next=tempp;
+        return head;
+
+    }
+
+     ListNode* solve(ListNode* head, int n) {
+        
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* first=dummy, *second=dummy;
+
+        for(int i=0;i<=n;i++){
+            first=first->next;
+        }
+
+        while(first){
+            second=second->next;
+            first=first->next;
+        }
+
+        ListNode* temp=second->next->next;
+
+        second->next=nullptr;
+        second->next=temp;
+
+        return dummy->next;
+     
+     }
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
 
-        if(!head->next){
-            return nullptr;
-        }
 
-        int count=0;
-        ListNode* temp=head;
-        while(temp){
-            count++;
-            temp=temp->next;
-        }
+        //return myApproach;
 
-        //if last node
-        count=count-n;
-        if(count==0){
-            return head->next;
-        }
-
-        
-        temp=head;
-        ListNode* ans=temp;
-        while(temp){
-            count--;
-            if(count==0 && temp->next){
-                ListNode* temper=temp->next->next;
-                temp->next=nullptr;
-                temp->next=temper;
-                temp=temper;
-            }
-            else{
-                temp=temp->next;
-            }
-
-        }
-        return ans;
-
+        return solve(head, n);
     }
 };
