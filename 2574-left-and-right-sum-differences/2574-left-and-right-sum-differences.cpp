67adc59a3e3dba6aacf07873eb1ea1e422070a53
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-06-06 14:48:44 +0530

    Time: 0 ms (100%), Space: 14.9 MB (94.53%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    vector<int> leftRightDifference(vector<int>& nums) {
+
+    vector<int> prefixSuffix(vector<int>& nums){
         
         vector<int> leftSum(nums.size()), rightSum(nums.size());
 
@@ -24,5 +24,32 @@ public:
 
         return ans;
 
+    }
+
+    vector<int> runningLeft(vector<int>& nums) {
+        
+        vector<int> rightSum(nums.size());
+        int sum=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            rightSum[i]=sum;
+            sum+=nums[i];
+        }
+
+
+        int leftSum=0;
+        vector<int>ans(nums.size());
+        for(int i=0;i<nums.size();i++){
+
+            ans[i]=abs(leftSum-rightSum[i]);
+            leftSum+=nums[i];
+        }
+        return ans;
+    }
+
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        //return prefixSuffix(nums);
+
+        return runningLeft(nums);
     }
 };
