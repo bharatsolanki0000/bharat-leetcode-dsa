327af3ca84661d4819cb34d9f47e744dd5ea9c1b
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-06-16 03:13:47 +0530

    Time: 0 ms (100%), Space: 11.4 MB (5.82%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,68 @@
+class Solution {
+
+    int brute(string s, int k){
+         int ans=0;
+        
+        for(int i=0;i<s.length();i++){
+           unordered_map<char,int> freq;
+           int maxFreq=-1;
+
+           for(int j=i;j<s.length();j++){
+
+                freq[s[j]]++;
+
+                maxFreq=max(maxFreq, freq[s[j]]); 
+                int totalElement=j-i+1;
+
+                int convertibleElements=totalElement-maxFreq;
+                if(convertibleElements<=k){
+                    ans=max(ans, totalElement);
+                }
+           }
+            
+        }
+        return ans;
+    }
+
+    int slidingWindow(string s, int k){
+        int i=0;
+        int j=0;
+        vector<int> freq(128);
+
+        int ans=-1;
+        int maxFreq=-1;
+        while(j<s.length()){
+            freq[s[j]]++;
+            maxFreq=max(maxFreq, freq[s[j]]);
+
+            
+            while((j-i+1)-maxFreq>k){
+                
+                freq[s[i]]--;
+               // maxFreq=-1;
+                // for(int k=0;k<128;k++){
+                //     maxFreq=max(maxFreq, freq[k]);
+                // }
+                i++;
+                
+            }
+            if(j-i+1-maxFreq<=k){
+                ans=max(ans, j-i+1);
+            }
+            j++;
+
+
+
+        }
+        return ans;
+    }
+
+    
+public:
+    int characterReplacement(string s, int k) {
+        //return brute(s,k);
+
+        return slidingWindow(s,k);
+       
+    }
+};
