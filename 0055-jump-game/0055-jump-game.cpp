d945f0a961a60eca0fe601eb3f13ee8d6f5cdf1d
Er.Bharat Solanki <151752291+bharatsolanki0000@users.noreply.github.com>
2026-06-22 03:02:20 +0530

    Time: 0 ms (100%), Space: 52.4 MB (26.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,106 +1,18 @@
 class Solution {
-
-    bool solve(vector<int>& nums, int index){
-
-        
-        if(index>=nums.size()){
-            return false;
-        }
-
-
-        for(int i=index+1;i<=index+nums[index];i++){
-
-            if(solve(nums,i)){
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    bool dpSolve(vector<int>& nums, int index, vector<int>&dp){
-
-        
-
-        if(index>=nums.size()){
-            return true;
-        }
-
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-
-        bool ans=false;
-        for(int i=index+1;i<=index+nums[index];i++){
-
-            if(dpSolve(nums,i,dp)){
-                ans= true;
-                break;
-            }
-        }
-
-        return dp[index]=ans;
-        
-    }
-
-    int tabuSolve(vector<int>& nums){
-        vector<int> dp(nums.size(),0);
-
-        dp[nums.size()-1]=1;
-
-        for(int index=nums.size()-2;index>=0;index--){
-                
-            bool ans=false;
-            for(int i=index+1;i<=index+nums[index];i++){
-
-                if(dp[i]){
-                    ans= true;
-                    break;
-                }
-            }
-
-            dp[index]=ans;
-        }
-
-        return dp[0];
-
-    }
-
-    bool greedySolve(vector<int>&nums){
+public:
+    bool canJump(vector<int>& nums) {
         int maxIndex=0;
 
-        for(int i=0;i<nums.size();i++){
+        int i=0;
 
-            
+        while(i<nums.size()){
+
             if(i>maxIndex){
                 return false;
             }
-            
             maxIndex=max(maxIndex, i+nums[i]);
-           
+            i++;
         }
         return true;
-
-    }
-
-
-public:
-    bool canJump(vector<int>& nums) {
-        
-        if(nums.size()==1){
-            return true;
-        }
-
-        //return solve(nums,0);
-
-        // vector<int> dp(nums.size()+1,-1);
-        // return dpSolve(nums,0,dp);
-
-
-        //return tabuSolve(nums);
-
-        //greedy
-
-        return greedySolve(nums);
     }
 };
